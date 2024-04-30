e45a515129ca3e8383cb4a4e9a01ad03c58d71e9
AJL <jeanlamarre16@yahoo.fr>
2024-04-30 10:48:46 +0200

    ajout de la sécu

--- back/src/main/java/com/mspr/back/config/SwaggerConfig.java
+++ back/src/main/java/com/mspr/back/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.mspr.back;
+package com.mspr.back.config;
 
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
--- back/src/main/java/com/mspr/back/repositories/UtilisateurRepository.java
+++ back/src/main/java/com/mspr/back/repositories/UtilisateurRepository.java
@@ -6,13 +6,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
-import java.util.Optional;
 
 @RepositoryRestResource(collectionResourceRel = "records", path = "utilisateurs")
 public interface UtilisateurRepository extends CrudRepository <Utilisateur, Long> {
     Utilisateur findByEmailAndPassword(String email, String password);
+    //Utiliser un optional pour bonne pratique
     Utilisateur findByEmail(String email);
 
+    //Utiliser un optional pour bonne pratique
     Utilisateur findUtilisateurById(Long id);
 
     List<Utilisateur> findAllByStatut(Statut statut);
